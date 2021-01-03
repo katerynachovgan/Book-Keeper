7db06b66c218a91d3fc9169522c44c2bf56f86d2
Kateryna Chovhan <chovganek@gmail.com>
2021-01-03 13:38:50 +0200

    feat: add show-modal function

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-URL');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+// Show Modal, Focus on Input
+function showModal() {
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+//Modal Event Listeners
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
